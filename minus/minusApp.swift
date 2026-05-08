c10ea237464a0f4383b9eedf4c7773cbf007ecc8
Isaac Serrano <isaac.serrano.-nd@disney.com>
2026-05-07 20:05:22 -0600

    Initial Commit

---
+++ minus/minusApp.swift
@@ -0,0 +1,17 @@
+//
+//  minusApp.swift
+//  minus
+//
+//  Created by Isaac Emmanuel Serrano Altamirano on 07/05/26.
+//
+
+import SwiftUI
+
+@main
+struct minusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
